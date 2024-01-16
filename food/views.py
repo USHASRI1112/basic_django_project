cb8a17b3edc9d55512ccaa109c793f60bf46a509
SARPANCH1 <anilkadurka@gmail.com>
2024-01-16 15:16:23 +0530

    last second commit

--- food/views.py
+++ food/views.py
@@ -4,18 +4,25 @@ from django.http import HttpResponse
 
 from .models import Item
 from django.template import loader
-
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 
-def index(request):
-    item_list = Item.objects.all()
-    # template=loader.get_template('food/index.html') #used when we want to use HttpResponse
-    context={
-        'item_list':item_list
-    }
-    # return HttpResponse(template.render(context,request)) 
-    return render(request,'food/index.html',context)
+# def index(request):
+#     item_list = Item.objects.all()
+#     # template=loader.get_template('food/index.html') #used when we want to use HttpResponse
+#     context={
+#         'item_list':item_list
+#     }
+#     # return HttpResponse(template.render(context,request)) 
+#     return render(request,'food/index.html',context)
+
+class IndexClassView(ListView):
+    model=Item 
+    template_name='food/index.html'
+    context_object_name='item_list'
 
 def item(request):
     return HttpResponse('<h1>This is an item view</h1>')
@@ -27,6 +34,11 @@ def detail(request,item_id):
     }
     return render(request,'food/detail.html',context)
 
+class FoodDetail(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
+    # context_object_name = 
+
 def create_item(request):
     form = ItemFom(request.POST or None)
 
@@ -34,6 +46,18 @@ def create_item(request):
         form.save()
         return redirect('food:index')
     return render(request,'food/item-form.html',{'form':form})
+
+#class based view for create item
+class CreateItem(CreateView):
+    model = Item 
+    fields = ['item_name','item_desc','item_price','item_image']
+    template_name = 'food/item-form.html'
+
+    def form_valid(self,form):
+        form.instance.user_name = self.request.user
+
+        return super().form_valid(form)
+    
 
 def update_item(request,id):
     item=Item.objects.get(pk=id)
